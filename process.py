ab46c8aee5318997891175371b133132153e5c46
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-04-13 23:58:13 -0300

    alterações na estrutura principal, pre implementação da função de comunicaçao do o arquivo xlsx.

--- process.py
+++ process.py
@@ -1,34 +1,31 @@
-import csv 
+from createTable import create_table
 from openpyxl import workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-arq = open('TESTE.csv', 'r')
-reader = csv.reader(arq)
-names = list()
-
-for line in reader:
-    for word in line:
-        x = word.split(';')
-        y = x[4].strip('"')
-        if y.upper() != 'NOME':
-            names.append(y)
-
 wb = load_workbook('Rotulo.xlsx')
 ws = wb['Envolope']
-positions = ['C5', 'H5', 'C12', 'H12', 'C19', 'H19', 'C26', 'H26', 'C33', 'H33']
+count = 0
 
-y = 0
-for i in range(0, int(len(names)), 1):
-    
-    if y == 10:
-        y = 0
-        string = 'perfil' + str(i) + '.xlsx' 
-        wb.save(string)
-    
-    print(y, i)
-    ws[positions[y]] = names[i] 
-    
-    y+=1
+n = 0
 
-string = 'perfil' + str(i) + '.xlsx' 
-wb.save(string)
+table = create_table()
+
+def te(n, table):
+    print(n)
+    positions = ['C5', 'H5', 'C12', 'H12', 'C19', 'H19', 'C26', 'H26', 'C33', 'H33']
+    for p in positions:
+        try:
+            ws[p] = table[n]
+            n+= 1
+        except:
+            print('erro')
+            break
+
+    string = 'Clientes' + str(n) + '.xlsx'
+    wb.save(string)
+    return n
+
+
+while count <= len(table):
+    n = te(n, table)
+    
