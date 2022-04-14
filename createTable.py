629999e9604c02dde23213697f2308a120cc7d58
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-04-13 22:09:55 -0300

    Divisão da criação do arquivo de dados em modulo

--- createTable.py
+++ createTable.py
@@ -1,29 +1,23 @@
-import csv 
+from catchCsv import catch_csv
 
-files = [str(x) for x in input().split(" ")]
-table = list()
+def create_table():
+    files = [str(x) for x in input().split(" ")]
+    table = list()
 
-#for file in files:
-arq = open('Arq01.csv', 'r')
-reader = csv.reader(arq)
+    for file in files:
+        data = catch_csv(file)
 
-data = list()
-for objectcsv in reader:
-    for i in objectcsv:
-        data.append(i)
+        y = data[0].replace('"', '')
+        header = y.split(';')
+        for line in range(1, len(data) - 1, 1):
+            x = data[line].replace('"', '')
+            words = x.split(';')
+            
+            row = dict().fromkeys(header, '')
 
-
-y = data[0].replace('"', '')
-header = y.split(';')
-for line in range(1, len(data) - 1, 1):
-    x = data[line].replace('"', '')
-    words = x.split(';')
-    
-    row = dict().fromkeys(header, '')
-
-    for w in range(1, len(header) - 1, 1):
-        row[header[w]] = words[w]
-    
-    table.append(row)
-    
-print(table[0])
+            for w in range(1, len(header) - 1, 1):
+                row[header[w]] = words[w]
+            
+            table.append(row)
+        
+    return table
