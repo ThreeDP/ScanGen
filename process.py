66541b9c45caf766a29316c7ad0c315eeeff235a
ThreeDP <davypaulinocbjr@outlook.com>
2022-04-14 08:40:31 -0300

    Implementação da função que realiza a inserção dos dados no modelo e salva o arquivo com até 10 clientes.

--- process.py
+++ process.py
@@ -6,26 +6,24 @@ wb = load_workbook('Rotulo.xlsx')
 ws = wb['Envolope']
 count = 0
 
-n = 0
-
 table = create_table()
 
 def te(n, table):
-    print(n)
     positions = ['C5', 'H5', 'C12', 'H12', 'C19', 'H19', 'C26', 'H26', 'C33', 'H33']
     for p in positions:
         try:
-            ws[p] = table[n]
-            n+= 1
-        except:
-            print('erro')
+            ws[p] = table[n]['Nome']
+            print(p, n)
+        except Exception as e:
+            print(e)
             break
-
+        n+= 1
+    
     string = 'Clientes' + str(n) + '.xlsx'
     wb.save(string)
     return n
 
-
-while count <= len(table):
-    n = te(n, table)
+while count < len(table):
+    count = te(count, table)
+    print(count)
     
