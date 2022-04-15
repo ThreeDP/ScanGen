22a5064b559b4a897d4e46d768354639cddac177
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-04-15 16:48:14 -0300

    Implementação do modulo de compreçao de arquivos

---
+++ compress.py
@@ -0,0 +1,9 @@
+from manageFiles import find_files
+import zipfile
+
+def compress(path):
+    patterns = zipfile.ZipFile(path + 'patterns.zip', 'w' )
+    for file in find_files(path):
+        patterns.write(path + file, compress_type=zipfile.ZIP_DEFLATED)
+
+    patterns.close()
