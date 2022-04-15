8f8f1ac6a4309aeb7462de68848bdc68e7b7c225
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-04-15 13:33:53 -0300

    Correção na geração da tabela de dados e inserção de comentários no código fixed #1

--- createTable.py
+++ createTable.py
@@ -11,17 +11,20 @@ def create_table():
         pathfile = path + file
         data = catch_csv(pathfile)
 
-        y = data[0].replace('"', '')
-        header = y.split(';')
-        for line in range(1, len(data) - 1, 1):
+        y = data[0].replace('"', '') # Elimina as aspas da string (Cabeçalho)
+        header = y.split(';') # Divide a string em um array (Cabeçalho)
+        for line in range(1, len(data), 1):
             x = data[line].replace('"', '')
             words = x.split(';')
             
-            row = dict().fromkeys(header, '')
+            row = dict().fromkeys(header, '') # Cria um dicionario vazio.
 
-            for w in range(1, len(header) - 1, 1):
-                row[header[w]] = words[w]
-            
-            table.append(row)
+            for column_index in range(0, len(header) - 1, 1): # Pula o cabeçalho e realiza o laço de inserção de dados.
+                    row[header[column_index]] = words[column_index] # Preenche o dicionário criado com os dados
+
+            for i in row.values(): # Valida se a row está vazia antes de inserir.
+                if i != '':
+                    table.append(row)
+                    break
         
-    return table
+    return len(table)
