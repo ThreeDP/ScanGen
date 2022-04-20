79cab93eddffcdf10a868393ed664bd5a208ef36
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2022-04-19 21:43:29 -0300

    Implementado função para criação de diretorios Implementação de função para deletar arquivos já existentes na pasta Implementação de Função para deletar pastas existentes

--- iosActions.py
+++ iosActions.py
@@ -1,4 +1,26 @@
 import os
 
 def find_files(local):
-    return os.listdir(local)
+    try:
+        return os.listdir(local)
+    except Exception as e:
+        print(e)
+
+def del_files(local, files):
+    for file in files:
+        os.remove(local + file)
+
+def make_dir(local):
+    if not os.path.isdir(local):
+        os.makedirs(local)
+        return
+    else:
+        files = find_files(local)
+        del_files(local, files)
+
+def remove_dir(local):
+    files = find_files(local)
+    del_files(local, files)
+    os.rmdir(local)
+
+remove_dir('./Arquivos/')
