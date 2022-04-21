05ea399ea577415b222ca2f2138e85270aadb6c9
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2022-04-21 13:23:32 -0300

    Implementação e import da função import_files e download do arquivo zip para o usuário

--- scanGen.py
+++ scanGen.py
@@ -1,11 +1,10 @@
-from manageFiles import create_table, insert_data
+from manageFiles import create_table, insert_data, import_files
 from compress import compress
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
 from iosActions import make_dir, remove_dir
 
 app = Flask(__name__)
-
-@app.route('/')
+@app.route('/', methods=["POST"])
 def scangen():    
     path = "./Arquivos/"
     path_patterns ="./Patterns/" 
@@ -13,20 +12,25 @@ def scangen():
     make_dir(path) # Cria uma pasta ou deleta os arquivos de uma existente.
     make_dir(path_patterns)
 
-    name_index = 0
     ## Função para import os arquivos
+    import_files(path)
+
+    name_index = 0
+    # Gera uma tabela com dos os valores dos arquivos importados.
     table = create_table(path)
 
+    # Agrupa os dados gerados em arquivos de 10 em 10.
     while name_index < len(table):
         name_index = insert_data(name_index, table, path_patterns)
 
-    compress('./')
+    # Realiza a compressão dos arquivos Gerados em um zip
+    zip_file = compress(path_patterns)
 
-    # Função para download dos arquivos.
     remove_dir(path) # Deleta os arquivos de uma existente e a pasta.
     remove_dir(path_patterns)
 
-    return 'teste'
+    # Retorna o arquivo .zip para o usuário.
+    return send_from_directory('./', zip_file, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
